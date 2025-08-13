54aec89e29819387290d7bfae1903dc35ac71919
adityadahiya12 <adityajaat623@gmail.com>
2025-08-13 18:34:15 +0530

    connect to mongono altas db

---
+++ src/index.js
@@ -0,0 +1,54 @@
+//require ('dotenv').config({path: './env'})
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: './.env' });
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express";
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        app.on("error", (error) => {
+            console.log("ERR: ", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})();*/
