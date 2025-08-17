6344bc7300499f92d2aa61bd0cd709197e38ec7f
adityadahiya12 <adityajaat623@gmail.com>
2025-08-17 21:36:18 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors({
@@ -13,4 +12,11 @@ app.use(express.urlencoded({extended:true, limit:"32kb"}))
 app.use(express.static("public"))
 
 
-export default app;
+// routes import 
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+app.use('/api/v1/users', userRouter);
+
+// http://localhost:8000/api/v1/users/register
+export { app } ;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
+
+
+const app = express();  // ✅ this line creates app
 
 dotenv.config({ path: './.env' });
 
