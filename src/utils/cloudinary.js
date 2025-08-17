5778fd9e0a059a655f0cbcaafbffa6ee61c4981e
adityadahiya12 <adityajaat623@gmail.com>
2025-08-17 12:43:15 +0530

    multer is setup as middleware in the system

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        const result = await cloudinary.uploader.upload(localFilePath,{resource_type: "auto"})
+        // file 
+        console.log("File uploaded successfully:", result);
+        return result;
+    } catch (error) {
+        console.error("Error uploading image:", response.url);
+        throw error;
+    }
+}
+
+cloudinary.v2.uploader.upload("path/to/image.jpg", (error, result) => {
+    if (error) {
+        console.error("Error uploading image:", error);
+    } else {
+        console.log("Image uploaded successfully:", result);
+    }
+});
